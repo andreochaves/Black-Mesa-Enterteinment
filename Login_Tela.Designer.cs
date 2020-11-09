902f8442b4d9d1cdc29b9c7ce814403d3b0e301a
Marco Patrocinio <marcopatrocinio@ucl.br>
2020-11-09 19:40:38 -0300

    Desbug

---
+++ Login_Tela.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Lets_Musics
+{
+    partial class Login_Tela
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Login_Tela
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Login_Tela";
+            this.Text = "Login_Tela";
+            this.Load += new System.EventHandler(this.Login_Tela_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
